4524497c612e25809301de9b6f477a280d72c5ab
Kelner Roberto <kelner250@hotmail.com>
2021-12-22 11:39:36 -0300

    modifica o ponto para vírgula nos preços das moedas

--- creepycoin/src/components/helpers/MapTheCurrencies.js
+++ creepycoin/src/components/helpers/MapTheCurrencies.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 const MapTheCurrencies = (currencies) => {
+  const replaceDot = (price) => {
+    const roundedNumb = `${Math.round(price * 100000) / 100000}`.replace(".", ",");
+    return roundedNumb;
+  }
+
   return (
   <table className="main-table">
     <thead>
@@ -19,7 +24,7 @@ const MapTheCurrencies = (currencies) => {
             <td className={changePercent24Hr > 0 ? 'positive-24' : 'negative-24'}>
             {Math.round(changePercent24Hr * 100) / 100}
             </td>
-            <td>{Math.round(priceUsd * 100000) / 100000}</td> 
+            <td>{`US$ ${replaceDot(priceUsd)}`}</td> 
           </tr>
         ))}
     </tbody>
